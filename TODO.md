755d05b59af60cf929e4a4193a0af13e0466e06a
Brian Wray <brian.wray@northwestern.edu>
2021-03-17 15:49:36 -0500

    read file and parse samples into tumor/normal lists

--- TODO.md
+++ TODO.md
@@ -1,6 +1,7 @@
-[ ] Read in a file
-[ ] Parse the file
-[ ] Separate the parsed lines into two lists, one for tumors and one for normals
+[x] Read in a file
+[x] Parse the file
+[x] Separate the parsed lines into two lists, one for tumors and one for normals
 [ ] Determine if the file is tumor-only
+[ ] Optional: generate script to make panel of normals
 [ ] Generate a gatk mutect2 command containing the parsed lines
 [ ] Generate temporary slurm script containing the mutect2 command
--- main.py
+++ main.py
@@ -1,121 +1,62 @@
-#!/usr/bin/python
-
 from os import mkdir
 import argparse
 import pathlib
 import time
 import string
-from jinja2 import Environment, FileSystemLoader
+# from jinja2 import Environment, FileSystemLoader
 
 
 def init_argparse():
     parser = argparse.ArgumentParser(
-            prog="python auto_proj_builder.py",
-            usage="%(prog)s --author 'name_string' --project_title \
-'project_string'",
-            description="Creates readme.md and main.py, filled with some \
-                         boilerplate, to new directory project_name\n\n"
+            prog="python auto_run_mutect2.py",
+            usage="%(prog)s --sample_base 'sample_base'",
+            description="Creates, and submits to slurm, a script for \
+                         running mutect2 based on the number of tumor/normal \
+                         samples"
             )
     parser.add_argument(
             "-v", "--version", action="version",
             version="{parser.prog} version 1.0.0"
             )
     parser.add_argument(
-            "-a", "--author", action='store',
-            help="The name of the author for this new project, in quotes. \
-                    Max 40 chars."
+            "-s", "--sample_base", action='store',
+            help="The sample base."
             )
     parser.add_argument(
-            "-p", "--project_title",
-            help="The title of this new project, max 40 chars, no whitespace."
-            )
-    parser.add_argument(
-            "-d", "--description", action='store', default="",
-            help="A single-line descrption of the project."
+            "-r", "--reference", action='store',
+            default="hg38",
+            help="The reference genome."
             )
     return parser
-
-
-def render_output(project_path,
-                  template_path,
-                  template_filename,
-                  output_filename,
-                  argument_dict):
-
-    # Environment for jinja
-    env = Environment(
-            loader=FileSystemLoader(template_path)
-            )
-
-    # Set up some paths
-    template_path = template_path + template_filename
-    output_path = project_path + output_filename
-
-    # Open and read our template
-    _template = env.get_template(template_filename)
-    template_str = open(template_path, mode='r').read()
-
-    # Process the template into our new output
-    new_output_str = _template.render(project_name=argument_dict["proj_title"],
-                                      author_name=argument_dict["author"],
-                                      description=argument_dict["description"])
-
-    # print(f"Creating readme in {project_path}")
-    with open(output_path, "w") as new_file:
-        new_file.write(new_output_str)
-    pass
-
-
-def is_valid_input_format(arg_type, argument):
-    if len(argument) > 40:
-        print(f"\nError: the {arg_type} argument you entered ({argument}) exceeds \
-the max length of 40 (by {len(argument)-40}). Try again.")
-        return False
-
-    if arg_type == "project_title":
-        for char in argument:
-            if char in string.whitespace:
-                print("\nError: no white space allowed in the project name \
-({project_name}). Try again")
-                return False
-    return True
 
 
 def main():
     parser = init_argparse()
     args = parser.parse_args()
 
-    author_name = args.author
-    if not is_valid_input_format("author", author_name):
-        return -1
+    sample = args.sample_base
+    filename = "data/reads/samples/" + sample + ".txt"
 
-    project_title = args.project_title
-    if not is_valid_input_format("project_title", project_title):
-        return -1
+    normals_list = list()
+    tumors_list = list()
 
-    proj_description = args.description
+    with open(filename) as f:
+        sample_files = f.readlines()
 
-    arg_dict = {"author": author_name, "proj_title": project_title,
-                "description": proj_description}
+    print(f"This is sample {sample}")
+    print(f"The filename is {filename}")
+    for line in sample_files:
+        if "P1" in line or "P2" in line:
+            tumors_list.append(line)
+        elif "PBL" in line:
+            normals_list.append(line)
+        else:
+            print(f"Watch out, {line} doesn't have P1, P2, or PBL in the \
+                  sample name.")
 
-    current_wd = pathlib.Path().absolute()
-    parent_path = str(current_wd.parent)
+    print(f"There are {len(normals_list)} normals and {len(tumors_list)} tumors")
+    print()
 
-    new_dir_path = parent_path + "/" + project_title + "_" + \
-        str(time.time()) + "/"
-
-    print(f"Creating project {project_title} by {author_name}")
-    print(f"Creating directory {new_dir_path}")
-    mkdir(new_dir_path)
-
-    template_path = str(current_wd) + "/templates/"
-
-    render_output(new_dir_path, template_path, "README_template.md",
-                  "README.md", arg_dict)
-    render_output(new_dir_path, template_path, "main_template.py",
-                  "main.py", arg_dict)
-    render_output(new_dir_path, template_path, "todo_template.md",
-                  "TODO.md", arg_dict)
 
 if __name__ == "__main__":
     main()
